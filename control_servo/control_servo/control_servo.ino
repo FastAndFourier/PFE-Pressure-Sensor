6b8d37a4d1c8f00e253a015e7e1a98eb68d524e1
LouisSimon <louiss78@outlook.com>
2022-01-25 13:03:31 +0100

    servo control and accelerometer measures arduino

--- control_servo/control_servo/control_servo.ino
+++ control_servo/control_servo/control_servo.ino
@@ -2,42 +2,177 @@
 
 Servo pan;
 Servo tilt;
-int pos_pan = 0;
-int pos_tilt = 0;
 
-int ind_sep;
+int i_pan = 0;
+int i_tilt = 0;
 
-String incomingByte;
+String measures;
 
+String flag_data;
+
+
+//****************************************//
+
+#define DECLINATION -5.55 
+
+#define PRINT_CALCULATED  //print calculated values
+
+// Call of libraries
+#include <Wire.h>
+#include <SparkFunLSM9DS1.h>
+
+// defining module addresses
+#define LSM9DS1_AG 0x6B //accelerometer and gyroscope
+
+LSM9DS1 imu; // Creation of the object
 
 void setup() {
+
+  // Setup Servomotors
   // initialize serial communication:
-  Serial.begin(9600);
+  Serial.begin(115200);
   // initialize the LED pin as an output:
-  pan.attach(8);
+  pan.attach(32);
   pan.write(0);
 
-  tilt.attach(9);
+  tilt.attach(33);
   tilt.write(0);
+
+
+  // Setup accelerometer
+  Wire.begin();     //initialization of the I2C communication
+  imu.settings.device.commInterface = IMU_MODE_I2C; // initialization of the module
+  imu.settings.device.agAddress = LSM9DS1_AG;
+  if (!imu.begin()) //display error message if that's the case
+  {
+    Serial.println("Communication problem.");
+    while (1);
+  }
+
 
 }
 
 void loop() {
+  
+  control_servo();
 
+  /*
+  if(Serial.available()>0){
+    flag_data = Serial.readStringUntil('\n');
+    if (flag_data.equals("send_data")){
+      print_servo();
+    }
+  }*/
+  
+  
+
+
+}
+
+void control_servo(){
+
+  String incomingByte;
+  int ind_sep;
+  int pos_pan_, pos_tilt_;
+  
   
   if (Serial.available()){
     incomingByte = Serial.readStringUntil('\n');
     ind_sep = incomingByte.indexOf('/');
 
+    pos_pan_ = incomingByte.substring(0,ind_sep).toInt();
+    pos_tilt_ = incomingByte.substring(ind_sep+1,incomingByte.length()).toInt();
     
-    pos_pan = incomingByte.substring(0,ind_sep).toInt();
-    pos_tilt = incomingByte.substring(ind_sep+1,incomingByte.length()).toInt();
+    pan.write(pos_pan_);
+    tilt.write(pos_tilt_);
+    /*
+    if( imu.accelAvailable() ){
+      imu.readAccel(); //measure with the accelerometer
+    }*/
+
+    Serial.print("r/");
+    /*
+    if(imu.calcAccel(imu.ax) > 0){
+      Serial.print('+');
+    }
+    Serial.print(imu.calcAccel(imu.ax),2);
+    Serial.print("/");
+    if(imu.calcAccel(imu.ay) > 0){
+      Serial.print('+');
+    }
+    Serial.print(imu.calcAccel(imu.ay),2);
+    Serial.print("/");
+    if(imu.calcAccel(imu.az) > 0){
+      Serial.print('+');
+    }
+    Serial.print(imu.calcAccel(imu.az),2);
+    Serial.print("//");*/
+
     
-    pan.write(pos_pan);
-    tilt.write(pos_tilt);
-    
-    Serial.print(incomingByte);
   }
 
-
 }
+
+void print_accel(){
+  
+  Serial.print("Accel/");
+  Serial.print(imu.calcAccel(imu.ax),2);
+  Serial.print("/");
+  Serial.print(imu.calcAccel(imu.ay),2);
+  Serial.print("/");
+  Serial.print(imu.calcAccel(imu.az),2);
+  Serial.print("//");
+}
+
+void print_servo(){
+  Serial.print("Servo/");
+  String pos_pan = String(pan.read());
+  String pos_tilt = String(tilt.read());
+  
+  for(int k=0; k<= 2-pos_pan.length(); k++){
+    Serial.print("0");
+  }
+  Serial.print(pos_pan+'/');
+  
+  for(int k=0; k<= 2-pos_tilt.length(); k++){
+    Serial.print("0");
+  }
+  Serial.println(pos_tilt);
+}
+
+void display_accel(){
+  Serial.print(-2);   //calculate acceleration
+  Serial.print(", ");
+  Serial.print(2);   //calculate acceleration
+  Serial.print(", ");
+  Serial.print(imu.calcAccel(imu.ax), 2);   //calculate acceleration
+  Serial.print(", ");
+  Serial.print(imu.calcAccel(imu.ay), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcAccel(imu.az), 2);
+  Serial.print("\n");
+}
+
+/*
+void printAccel()
+{
+#ifdef PRINT_CALCULATED
+  Serial.print(-2);   //calculate acceleration
+  Serial.print(", ");
+  Serial.print(2);   //calculate acceleration
+  Serial.print(", ");
+  Serial.print(imu.calcAccel(imu.ax), 2);   //calculate acceleration
+  Serial.print(", ");
+  Serial.print(imu.calcAccel(imu.ay), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcAccel(imu.az), 2);
+  Serial.print("\n");                    //measured in g
+#elif defined PRINT_RAW
+  Serial.print(imu.ax);                   //or display raw data
+  Serial.print(", ");
+  Serial.print(imu.ay);
+  Serial.print(", ");
+  Serial.println(imu.az);
+#endif
+}
+*/
--- control_servo/test.py
+++ control_servo/test.py
@@ -2,42 +2,38 @@ import serial
 import time
 import numpy as np
 
-def blink(arduino,frequency):
-    
-    time_start = time.time()
-    total_time = 0
+def servo_full_sweep(arduino,print_=False,sleep=0):
 
-    while total_time < 10:
-        arduino.writelines(b'H')
-        time.sleep(1/(2*frequency))
-        #print("in")
-        arduino.writelines(b'L')
-        total_time = time.time() - time_start
-        
+    #time.sleep(1)
+    # Init each servo to 0
+    arduino.write(('0/0\n').encode())  
+
+    arduino.readline()
+
+    for pos_pan in range(20,130,10):
+        for pos_tilt in range(50,130,10):
+            pos = str(pos_pan) + '/' + str(pos_tilt) + '\n'
+            arduino.write(pos.encode())
+            arduino.read(4).decode()
+            #arduino.read(5).decode()
+            #print(arduino.read(5).decode().split('\r\n')[0])
+            #arduino.readline()
+
+            # while measure!="received":
+            #     measure = arduino.read(8).decode().split('\r\n')[0]
+
+            time.sleep(.1)
+            
+            
+            
+    arduino.write(('0/0\n').encode()) 
+
+
+
 
 if __name__ == "__main__":
 
-    serial_port = serial.Serial(port="COM3", baudrate = 9600,timeout=1)
+    serial_port = serial.Serial(port="COM4", baudrate = 115200,timeout=0.9)
 
-    # # réinitialisation
-    # serial_port.setDTR(False)
-    # time.sleep(0.1)
-    # serial_port.setDTR(True)
-
-    # # on vide le buffer
-    # serial_port.flushInput()
-
-    serial_port.write(('0\n').encode())    
-
-    for pos_pan in range(20,150,10):
-        for pos_tilt in range(50,130,10):
-
-            pos = str(pos_pan) + '/' + str(pos_tilt) + '\n'
-
-            serial_port.write(pos.encode())
-        
-            read_data = serial_port.readlines()
-            if len(read_data)!=0:
-                print(read_data[0].decode())
-
+    servo_full_sweep(serial_port,print_=False,sleep=1)
     serial_port.close()
