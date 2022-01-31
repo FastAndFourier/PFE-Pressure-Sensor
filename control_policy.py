5cd3b993161481cc73448a354b82ef2bd14b36d6
LouisSimon <louiss78@outlook.com>
2022-01-31 11:31:04 +0100

    updates on behavioral cloning

--- control_policy.py
+++ control_policy.py
@@ -1,11 +1,12 @@
-import numpy as np
 import torch
+import numpy as np
+import matplotlib.pyplot as plt
 import torch.nn as nn
-import torch.nn.functional as F
+from torch.utils.data import DataLoader
 
 class customDataset(torch.utils.data.Dataset):
 
-    def __init__(self,path_action,path_obs) -> None:
+    def __init__(self,path_action,path_obs,augmentData=False) -> None:
 
         self.action = np.load(path_action[0])
         if len(path_action) > 1:
@@ -21,8 +22,11 @@ class customDataset(torch.utils.data.Dataset):
         self.zero = np.load(path_obs[0])[0]
         self.transform = None
 
+        if augmentData:
+            self.dataAugmentation()
+
     def __len__(self):
-        return len(self.zero)
+        return len(self.obs)
 
     def __getitem__(self,idx):
 
@@ -36,6 +40,12 @@ class customDataset(torch.utils.data.Dataset):
 
 
         return sample
+
+    def dataAugmentation(self):
+
+        for k in range(len(self.obs)):
+            self.obs = np.concatenate((self.obs,np.fliplr(self.obs[k])[np.newaxis]),axis=0)
+            self.action = np.concatenate((self.action,self.action[k][::-1][np.newaxis]),axis=0)
         
 
 class policyNet(nn.Module):
@@ -48,16 +58,89 @@ class policyNet(nn.Module):
         
     def forward(self,x):
         
-        x = F.relu(self.lin1(x))
-        x = F.relu(self.lin2(x))
-        x = F.tanh(self.lin3(x))
+        x = torch.relu(self.lin1(x))
+        x = torch.relu(self.lin2(x))
+        x = torch.tanh(self.lin3(x))
 
         return x
 
-def predict(obs,policy):
 
-    obs = torch.tensor(obs).float()
-    action = policy(obs).detach().numpy()
-    action = [action[0]*10,action[1]*10]
+def imitation(dataset,epochs,learning_rate):
 
-    return predict
+    policy = policyNet()
+    
+    loss_func = nn.MSELoss()
+    
+    optimizer = torch.optim.SGD(policy.parameters(), lr=learning_rate, momentum=0.9)
+
+    loss_array = []
+
+    for epoch in range(epochs):
+        
+        running_loss = 0.0
+        for i, data in enumerate(dataset, 0):
+
+            inputs, labels = data
+
+            optimizer.zero_grad()
+            outputs = policy(inputs.float())
+            loss = loss_func(outputs,labels.float())
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        loss_array.append(running_loss/dataset.__len__())
+
+
+        if (epoch+1)%10==True:
+            print(f'[{epoch}/{epochs}] loss: {running_loss/dataset.__len__():.3f}')
+        
+
+
+    print(f'[{epoch + 1}/{epochs}] loss: {running_loss/dataset.__len__():.3f}')
+
+    return policy, loss_array
+
+if __name__ == "__main__":
+
+
+    epochs=1000
+    batch_size=16
+    augmentData = True
+
+    dataset = customDataset(["./action.npy","./action1.npy","./action2.npy","./action3.npy"],
+                            ["./observation.npy","./observation1.npy","./observation2.npy","./observation3.npy"],
+                            augmentData=augmentData)
+    
+
+    len_trainDataset = int(.7*len(dataset))
+
+    trainDataset, testDataset = torch.utils.data.random_split(dataset,[len_trainDataset,len(dataset)-len_trainDataset])
+
+    
+    print("Number of samples in the training set: ",trainDataset.__len__())
+
+    trainLoader = DataLoader(trainDataset,batch_size=batch_size,shuffle=True)
+    testLoader = DataLoader(testDataset,batch_size=batch_size,shuffle=True)
+
+    policy, loss_array = imitation(trainLoader,epochs=epochs,learning_rate=0.001)
+    torch.save(policy,"./expert_policy.pt")
+    policy = torch.load('./expert_policy.pt')
+    
+    test_loss = 0.0
+    loss_func = nn.MSELoss()
+    
+    with torch.no_grad():
+        for sample in testLoader:
+            obs, labels = sample
+            outputs = policy(obs.float())
+            temp =  loss_func(outputs,labels.float())
+            test_loss += temp.item()
+
+    print(f'Loss on test set {test_loss/testLoader.__len__():.3f}')
+
+    plt.figure()
+    plt.plot([round(loss_,2) for loss_ in loss_array])
+    plt.show()
+
